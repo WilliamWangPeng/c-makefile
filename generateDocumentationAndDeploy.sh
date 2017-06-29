3baf67139e62639392714beefaeb7b98fa15e339
laurelmcintyre <lmcintyre@lanl.gov>
2017-06-29 09:14:47 -0600

    add c-makefile

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -5,8 +5,8 @@ set -e
 mkdir code_docs
 cd code_docs
 
-git clone -b gh-pages https://github.com/laurelmcintyre/c c
-cd c
+git clone -b gh-pages https://github.com/laurelmcintyre/c-makefile c-makefile
+cd c-makefile
 
 git config --global push.default simple
 git config user.name "Travis CI"
